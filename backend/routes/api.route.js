46bef1af8f7e18e5b634ff0f54bd47fe9b3b5a36
Gabriel Fasching <gabefasching@gmail.com>
2022-10-15 01:11:26 -0400

    some more backend stuff

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+const Twitter = require('twitter')
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+})
+
+
+// gets pictures
+router.get('/pictures', async(req, req, next) => {
+    const ht = req.query.hashtag;
+    const pics = await client.get('search/tweets.json', {
+      ht,
+      filter:images
+
+    })
+    res.send(pics);
 });
+
+//  gets pictures
+ router.get('/', async(req, req, next) => {
+     res.send({ message: 'Ok api is working'});
+ });
 
 module.exports = router;
