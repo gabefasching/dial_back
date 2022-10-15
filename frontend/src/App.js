1b9639149c2ec011c53da66ea7d28816a0db2e16
Omer Kandemir <farukkandemir585@gmail.com>
2022-10-15 01:12:30 -0400

    front end update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import logo from './twitter.svg';
 import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [pictures, setPictures] = useState([])
+  const [woeid, setWoeid] = useState('1')
+
+  function listPictures() {
+    return <h2>Pictures</h2>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo" alt="twitter" /> 
+        <h3>Twitter Trends</h3>
       </header>
+      <div className="menu">
+          <select name="trending-pictures" onChange={e => alert(e.target.value)}>
+              <option value="retro">#retro</option>
+              <option value="vintage">#vintage</option>
+              <option value="retrogames">#retrogames</option>
+              <option value="arcadegames">#arcadegames</option>     
+          </select>
+      </div>
+      <div className="content">{listPictures()}</div>
     </div>
   );
 }
 
 export default App;
+
+
